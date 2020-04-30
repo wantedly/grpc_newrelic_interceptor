87f06244bdd090cc43c48b9fa2610615135be927
izumin5210 <m@izum.in>
2020-04-30 18:19:53 +0900

    Create segment for each ServerInterceptors

---
+++ lib/grpc_newrelic_interceptor/server_interceptor_ext.rb
@@ -0,0 +1,29 @@
+module GrpcNewrelicInterceptor::ServerInterceptor
+  module Ext
+    def request_response(request:, call:, method:)
+      return super if !newrelic_enabled?
+
+      begin
+        finishable = Tracer.start_segment(
+          name: newrelic_segment_name,
+        )
+        super
+      ensure
+        finishable.finish if finishable
+      end
+    end
+
+    private
+
+    # @return [bool]
+    def newrelic_enabled?
+      NewRelic::Agent.instance.started?
+    end
+
+    def newrelic_segment_name
+      @newrelic_segment_name ||= "#{self.class.name}/request_response"
+    end
+  end
+
+  ::GRPC::ServerInterceptor.prepend Ext
+end
